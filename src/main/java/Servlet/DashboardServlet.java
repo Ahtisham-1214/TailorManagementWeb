49978a7f92fb6144f4dc0335e286df6f02561d5a
YourGitHubUsername <YourEmail@example.com>
2025-04-21 22:40:29 +0500

    Dashboard servlet

--- src/main/java/Servlet/DashboardServlet.java
+++ src/main/java/Servlet/DashboardServlet.java
@@ -19,7 +19,36 @@ public class DashboardServlet extends HttpServlet {
             return;
         }
 
+        String role = (String) session.getAttribute("role");
+        String action = request.getParameter("action");
+
+        System.out.println("Role: " + role);
+        System.out.println("Action: " + action);
+
+        if ("price".equals(action)) {
+            if ("admin".equals(role)) {
+                System.out.println("Forwarding to /PricesServlet");
+                // Redirect to PricesServlet instead of directly to JSP
+                response.sendRedirect(request.getContextPath() + "/PricesServlet");
+                return;
+//                request.getRequestDispatcher("/Prices.jsp").forward(request, response);
+            }else {
+                response.sendRedirect(request.getContextPath() + "/Unauthorized.jsp");
+            }
+            return;
+        }
+        else if ("order".equals(action)) {
+            if ("admin".equals(role)) {
+                System.out.println("Forwarding to Coat.jsp");
+                request.getRequestDispatcher("Coat.jsp").forward(request, response);
+            } else {
+                response.sendRedirect(request.getContextPath() + "/Unauthorized.jsp");
+            }
+        } else {
+            request.getRequestDispatcher("/Index.jsp").forward(request, response);
+        }
+
         // Correct forwarding with absolute path
-        request.getRequestDispatcher("/Index.jsp").forward(request, response);
+        // request.getRequestDispatcher("/Index.jsp").forward(request, response);
     }
 }
