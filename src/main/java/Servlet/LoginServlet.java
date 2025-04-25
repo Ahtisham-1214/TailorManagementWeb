42078b5a2a9012cc8a305da3f25a0f63521296bf
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-04-25 20:28:01 +0500

    Added remove login page once logged in functionality

--- src/main/java/Servlet/LoginServlet.java
+++ src/main/java/Servlet/LoginServlet.java
@@ -20,6 +20,12 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res)
             throws ServletException, IOException {
+
+        // Prevent caching used to avoid go back to login page
+        res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+        res.setHeader("Pragma", "no-cache"); // HTTP 1.0
+        res.setDateHeader("Expires", 0); // Proxies
+
         if (req.getSession(false) != null && req.getSession().getAttribute("user") != null) {
             res.sendRedirect(req.getContextPath() + "/DashboardServlet");
             return;
