11ad5b3cb9418d2b6bb7f3e71542f363c644e58d
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-04-25 22:50:41 +0500

    for viewing jsp

---
+++ src/main/java/Servlet/ViewServlet.java
@@ -0,0 +1,25 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/ViewServlet")
+public class ViewServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+         response.sendRedirect(request.getContextPath() + "/LoginServlet");
+        }else {
+            request.getRequestDispatcher("View.jsp").forward(request, response);
+        }
+
+    }
+
+}
