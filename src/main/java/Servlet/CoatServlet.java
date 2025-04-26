dfe8613e706e6eaccb966fcddca4d620c1cdd32f
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-04-26 22:29:06 +0500

    Work remaining

--- src/main/java/Servlet/CoatServlet.java
+++ src/main/java/Servlet/CoatServlet.java
@@ -1,5 +1,8 @@
 package Servlet;
 
+import Backend.Coat;
+import Backend.Order;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,5 +22,24 @@ public class CoatServlet extends HttpServlet {
         }
 
         req.getRequestDispatcher("/WEB-INF/view/Coat.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        float chest = Float.parseFloat(req.getParameter("chest"));
+        float waist = Float.parseFloat(req.getParameter("waist"));
+        float sleeves = Float.parseFloat(req.getParameter("sleeves"));
+        float shoulder = Float.parseFloat(req.getParameter("shoulder"));
+        byte status = Byte.parseByte(req.getParameter("status"));
+        int quantity = Integer.parseInt(req.getParameter("quantity"));
+        java.sql.Date orderDate = java.sql.Date.valueOf(req.getParameter("order-date"));
+        java.sql.Date deliveryDate = java.sql.Date.valueOf(req.getParameter("delivery-date"));
+        String description = req.getParameter("description");
+
+        new Order().getOrders().getFirst().getCoats().add(
+                new Coat(chest, waist, sleeves, shoulder, status, description, quantity, orderDate, deliveryDate));
+        System.out.println("Coat added successfully");
+
     }
 }
