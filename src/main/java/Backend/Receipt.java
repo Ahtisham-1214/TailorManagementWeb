d7984661257f78319f43e0c5d7202f9cb8c17817
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-04-27 19:17:25 +0500

    Test successfull, work remaining

--- src/main/java/Backend/Receipt.java
+++ src/main/java/Backend/Receipt.java
@@ -1,8 +1,13 @@
 package Backend;
 
+import java.util.LinkedList;
+
+
 public class Receipt {
     private ShopDetails shopDetails;
     private Order order;
+    private LinkedList<Item> items;
+    private Prices prices;
     String shopName;
     String shopAddress;
     String shopPhone;
@@ -11,12 +16,23 @@ public class Receipt {
     String customerName;
 
     public Receipt() {
+
     shopDetails = new ShopDetails();
     order = new Order();
+    prices = new Prices();
+    items = new LinkedList<>();
     order.addOrder(new Customer("Ahtisham", "12345678901"));
     order.getOrders().getFirst().getCoats().add(
             new Coat(12, 12, 12, 12, (byte) 1,
                     "hello", 10, null, null));
+
+
+
+    if (!order.getOrders().getFirst().getCoats().isEmpty()){
+        items.add(new Item("Coat",
+                order.getOrders().getFirst().getCoats().getFirst().getQuantity(),
+                prices.getCoatPrice()));
+    }
 
 
     this.shopAddress = shopDetails.getAddress();
@@ -24,6 +40,10 @@ public class Receipt {
     this.shopPhone = shopDetails.getPhone();
     this.shopEmail = shopDetails.getEmail();
     this.customerName = order.getOrders().getFirst().getName();
+    }
+
+    public LinkedList<Item> getItems() {
+        return items;
     }
 
     public String getShopName() {
