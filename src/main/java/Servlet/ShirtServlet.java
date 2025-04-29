33428a4aa353cec151a8d2db8d2faf4b783f7d92
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-04-29 22:34:00 +0500

    For handling shirt form

---
+++ src/main/java/Servlet/ShirtServlet.java
@@ -0,0 +1,22 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/ShirtServlet")
+public class ShirtServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/LoginServlet");
+        }
+        req.getRequestDispatcher("/WEB-INF/view/Shirt.jsp").forward(req, resp);
+    }
+}
