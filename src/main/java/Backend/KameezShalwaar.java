7694c196184894bbeed452df4ad062af15dc8fb9
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-05-04 16:39:46 +0500

    Added validation

--- src/main/java/Backend/KameezShalwaar.java
+++ src/main/java/Backend/KameezShalwaar.java
@@ -1,17 +1,19 @@
 package Backend;
 
 import java.util.Date;
+
 public class KameezShalwaar implements Measurement {
     private float trouserLength;
     private byte trouserType; // 1 for Shalwaar 2 for pajama
     private float trouserAnkle;
 
     private float kameezLength;
-    private float sleeves;
+    private float sleevesLength;
     private byte cuffType; // 1 for Square, 2 for Round cuff
     private byte kameezType; // 1 for Square, 2 for Round Daman
     private float shoulder;
     private float neck;
+    private float chest;
     private byte collarType; // 1 for Cooper, 2 for french 3 for sherwani
     private String description;
     private int quantity;
@@ -19,112 +21,192 @@ public class KameezShalwaar implements Measurement {
     private Date deliveryDate;
     private byte status; // 1 for pending, 2 for progress, 3 for completed, 4 for delivered
 
-    public byte getStatus() {
-        return status;
+    public KameezShalwaar(float trouserLength, byte trouserType,
+                          float trouserAnkle, float kameezLength, float chest,
+                          float sleevesLength, byte cuffType, byte kameezType,
+                          float shoulder, float neck, byte collarType,
+                          byte status, String description, int quantity,
+                          Date orderDate, Date deliveryDate) {
+        this.setCollarType(collarType);
+        this.setKameezLength(kameezLength);
+        this.setKameezType(kameezType);
+        this.setNeck(neck);
+        this.setChest(chest);
+        this.setDescription(description);
+        this.setOrderDate(orderDate);
+        this.setShoulder(shoulder);
+        this.setStatus(status);
+        this.setTrouserAnkle(trouserAnkle);
+        this.setTrouserLength(trouserLength);
+        this.setSleevesLength(sleevesLength);
+        this.setTrouserType(trouserType);
+        this.setCuffType(cuffType);
+        this.setDeliveryDate(deliveryDate);
+        this.setQuantity(quantity);
+    }
+
+    public void setChest(float chest) {
+        if (chest < 1){
+            throw new IllegalArgumentException("Chest must be greater than 0");
+        }
+        this.chest = chest;
     }
 
     public void setStatus(byte status) {
+        if (status < 1) {
+            throw new IllegalArgumentException("Status Should be greater than 1");
+        }
         this.status = status;
     }
 
-    public void setDeliveryDate(Date deliveryDate) {
-        this.deliveryDate = deliveryDate;
+    public void setQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("Quantity Should at least be 1");
+        }
+        this.quantity = quantity;
+    }
+
+    public void setTrouserLength(float trouserLength) {
+        if (trouserLength < 1) {
+            throw new IllegalArgumentException("Trouser Length Should be greater than 0");
+        }
+        this.trouserLength = trouserLength;
+    }
+
+    public void setCuffType(byte cuffType) {
+        if (cuffType < 1) {
+            throw new IllegalArgumentException("Cuff Type Should be greater than 0");
+        }
+        this.cuffType = cuffType;
+    }
+
+    public void setTrouserType(byte trouserType) {
+        if (trouserType < 1) {
+            throw new IllegalArgumentException("Trouser Type Should be greater than 0");
+        }
+        this.trouserType = trouserType;
+    }
+
+    public void setKameezType(byte kameezType) {
+        if (kameezType < 1) {
+            throw new IllegalArgumentException("Kameez Type Should be greater than 0");
+        }
+        this.kameezType = kameezType;
+    }
+
+    public void setKameezLength(float kameezLength) {
+        if (kameezLength < 1) {
+            throw new IllegalArgumentException("Kameez Length Should be greater than 0");
+        }
+        this.kameezLength = kameezLength;
+    }
+
+    public void setSleevesLength(float sleevesLength) {
+        if (sleevesLength < 1) {
+            throw new IllegalArgumentException("Sleeve Length Should be greater than 0");
+        }
+        this.sleevesLength = sleevesLength;
+    }
+
+    public void setTrouserAnkle(float trouserAnkle) {
+        if (trouserAnkle < 1) {
+            throw new IllegalArgumentException("Trouser Ankle Should be greater than 0");
+        }
+        this.trouserAnkle = trouserAnkle;
+    }
+
+    public void setNeck(float neck) {
+        if (neck < 1) {
+            throw new IllegalArgumentException("Neck Should be greater than 0");
+        }
+        this.neck = neck;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setShoulder(float shoulder) {
+        if (shoulder < 1) {
+            throw new IllegalArgumentException("Shoulder Should be greater than 0");
+        }
+        this.shoulder = shoulder;
+    }
+
+    public void setCollarType(byte collarType) {
+        if (collarType < 1) {
+            throw new IllegalArgumentException("Collar Type Should be greater than 0");
+        }
+        this.collarType = collarType;
     }
 
     public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
+        if (orderDate == null) {
+            System.out.println("Order Date for Kameez Shalwaar was null but set today date");
+            this.orderDate = java.sql.Date.valueOf(java.time.LocalDate.now());
+        } else
+            this.orderDate = orderDate;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
+    public void setDeliveryDate(Date deliveryDate) {
+        if (deliveryDate == null) {
+            this.deliveryDate = java.sql.Date.valueOf(java.time.LocalDate.now().plusDays(7));
+            System.out.println("Delivery Date for Kameez Shalwaar was null but set to " +
+                    this.getDeliveryDate());
+        } else
+            this.deliveryDate = deliveryDate;
+    }
+
+    public byte getStatus() {
+        return status;
     }
 
     public float getTrouserLength() {
         return trouserLength;
     }
 
-    public void setTrouserLength(float trouserLength) {
-        this.trouserLength = trouserLength;
-    }
-
     public byte getTrouserType() {
         return trouserType;
-    }
-
-    public void setTrouserType(byte trouserType) {
-        this.trouserType = trouserType;
     }
 
     public float getTrouserAnkle() {
         return trouserAnkle;
     }
 
-    public void setTrouserAnkle(float trouserAnkle) {
-        this.trouserAnkle = trouserAnkle;
+    public float getChest() {
+        return chest;
     }
 
     public float getKameezLength() {
         return kameezLength;
     }
 
-    public void setKameezLength(float kameezLength) {
-        this.kameezLength = kameezLength;
-    }
-
-    public float getSleeves() {
-        return sleeves;
-    }
-
-    public void setSleeves(float sleeves) {
-        this.sleeves = sleeves;
+    public float getSleevesLength() {
+        return sleevesLength;
     }
 
     public byte getCuffType() {
         return cuffType;
     }
 
-    public void setCuffType(byte cuffType) {
-        this.cuffType = cuffType;
-    }
-
     public byte getKameezType() {
         return kameezType;
-    }
-
-    public void setKameezType(byte kameezType) {
-        this.kameezType = kameezType;
     }
 
     public float getShoulder() {
         return shoulder;
     }
 
-    public void setShoulder(float shoulder) {
-        this.shoulder = shoulder;
-    }
-
     public float getNeck() {
         return neck;
-    }
-
-    public void setNeck(float neck) {
-        this.neck = neck;
     }
 
     public byte getCollarType() {
         return collarType;
     }
 
-    public void setCollarType(byte collarType) {
-        this.collarType = collarType;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public int getQuantity() {
@@ -140,46 +222,23 @@ public class KameezShalwaar implements Measurement {
     }
 
 
-    public KameezShalwaar(float trouserLength, byte trouserType,
-                          float trouserAnkle, float kameezLength,
-                          float sleeves, byte cuffType, byte kameezType,
-                          float shoulder, float neck, byte collarType,
-                          byte status, String description, int quantity,
-                          Date orderDate, Date deliveryDate) {
-        this.trouserLength = trouserLength;
-        this.trouserType = trouserType;
-        this.trouserAnkle = trouserAnkle;
-        this.kameezLength = kameezLength;
-        this.sleeves = sleeves;
-        this.cuffType = cuffType;
-        this.kameezType = kameezType;
-        this.shoulder = shoulder;
-        this.neck = neck;
-        this.collarType = collarType;
-        this.status = status;
-        this.description = description;
-        this.quantity = quantity;
-        this.deliveryDate = deliveryDate;
-        this.orderDate = orderDate;
-    }
-
     @Override
     public String toString() {
         return "KameezShalwaar{" +
-            "trouserLength=" + trouserLength +
-            " trouserType=" + (trouserType == 1 ? "Shalwaar" : "Pajama") +
-            " trouserAnkle=" + trouserAnkle + "\n" +
-            " kameezLength=" + kameezLength +
-            " sleeves=" + sleeves + "\n" +
-            " cuffType=" + (cuffType == 1 ? "Square" : "Round cuff") +
-            " kameezType=" + (kameezType == 1 ? "Square Daman" : "Round Daman") +
-            " shoulder=" + shoulder + "\n" +
-            " neck=" + neck +
-            " collarType=" + (collarType == 1 ? "Cooper" : collarType == 2 ? "French" : "Sherwani") +
-            " description='" + description + '\'' +
-            " quantity=" + quantity + "\n" +
-            " orderDate=" + orderDate +
-            " deliveryDate=" + deliveryDate +
-            '}';
+                "trouserLength=" + trouserLength +
+                " trouserType=" + (trouserType == 1 ? "Shalwaar" : "Pajama") +
+                " trouserAnkle=" + trouserAnkle + "\n" +
+                " kameezLength=" + kameezLength +
+                " sleevesLength=" + sleevesLength + "\n" +
+                " cuffType=" + (cuffType == 1 ? "Square" : "Round cuff") +
+                " kameezType=" + (kameezType == 1 ? "Square Daman" : "Round Daman") +
+                " shoulder=" + shoulder + "\n" +
+                " neck=" + neck +
+                " collarType=" + (collarType == 1 ? "Cooper" : collarType == 2 ? "French" : "Sherwani") +
+                " description='" + description + '\'' +
+                " quantity=" + quantity + "\n" +
+                " orderDate=" + orderDate +
+                " deliveryDate=" + deliveryDate +
+                '}';
     }
 }
