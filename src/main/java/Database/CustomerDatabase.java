89662e5abede30989ea066440a8f926d7086e6dc
Ahtisham-1214 <ahtishamshaikh1214@gmail.com>
2025-05-07 22:11:24 +0500

    Added Customer database class

---
+++ src/main/java/Database/CustomerDatabase.java
@@ -0,0 +1,43 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class CustomerDatabase {
+    private final Connection connection;
+
+    public CustomerDatabase(String name, String phone) throws SQLException {
+        this.connection = DatabaseConnection.getConnection();
+        insertCustomer(name, phone);
+    }
+
+
+
+    private void insertCustomer(String name, String phone) throws SQLException {
+        try {
+            String query = "INSERT INTO customer VALUES (?, ?)";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, name);
+            statement.setString(2, phone);
+
+            statement.executeUpdate();
+            if (statement.getUpdateCount() == 0) {
+                throw new SQLException("Insert Customer Failed");
+            }
+        }catch (SQLException e){
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    // For Testing
+    public static void main(String[] args) {
+//        CustomerDatabase cd = new CustomerDatabase();
+//
+//        try {
+//            System.out.println(cd.insertCustomer("Ahtisham", "03448143397"));
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+    }
+}
